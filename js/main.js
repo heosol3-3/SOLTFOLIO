0a4a63286efbb667b6362c4299f8ba7f1c0d30aa
heosol3-3 <samz3@naver.com>
2023-05-12 17:01:09 +0900

    edit 2

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $(".title").lettering();
+  $(".title_main").lettering();
   $(".button_join").lettering();
 });
 
@@ -16,7 +16,7 @@ $(document).ready(function() {
 function animation() {
   var title1 = new TimelineMax();
   title1.to(".button_join", 0, {visibility: 'hidden', opacity: 0})
-  title1.staggerFromTo(".title span", 0.5, 
+  title1.staggerFromTo(".title_main span", 0.5, 
   {ease: Back.easeOut.config(1.7), opacity: 0, bottom: -80},
   {ease: Back.easeOut.config(1.7), opacity: 1, bottom: 0}, 0.05);
   title1.to(".button_join", 0.2, {visibility: 'visible' ,opacity: 1})
